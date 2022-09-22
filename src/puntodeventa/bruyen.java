3d22a7d019726d8d6040b83e92e4cbf218a316df
facuufigueroa <figueroafacundo123@gmail.com>
2022-09-22 09:36:39 -0300

    Se creo conexion base de datos con MYSQL

--- src/puntodeventa/bruyen.java
+++ src/puntodeventa/bruyen.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package puntodeventa;
 
-/**
- *
- * @author Facundo
- */
+import DataBase.ConexionBD;
+import View.MenuPrincipalView;
+
+
 public class bruyen {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+       MenuPrincipalView menu = new MenuPrincipalView();
+       menu.setVisible(true);
+       ConexionBD conex = new ConexionBD();
+       ConexionBD.getConnection();
     }
     
 }
